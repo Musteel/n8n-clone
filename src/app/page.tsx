afba8d0de998842ec71ee931f828904d7154700e
Mustafa <mustafa_9611@hotmail.com>
2025-11-01 12:55:37 +0100

    ai provider added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,12 @@ const Page = () => {
   const queryClient = new QueryClient();
   const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
+  const testAI = useMutation(trpc.testAi.mutationOptions({
+    onSuccess: () => {
+      toast.success('AI job is queued');
+    }
+  }));
+
   const create = useMutation(trpc.createWorkflow.mutationOptions({
     onSuccess: () => {
       toast.success('Job queued');
@@ -24,6 +30,7 @@ const Page = () => {
       <div>
       {JSON.stringify(data, null, 2)}
       </div>
+      <Button disabled={testAI.isPending} onClick={() => testAI.mutate()}>Test AI</Button>
       <Button disabled={create.isPending} onClick={() => create.mutate()}>Create workflow</Button>
       <LogoutButton />
     </div>
