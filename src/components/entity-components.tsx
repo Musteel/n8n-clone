4527b64aca121d23456082965793fc29efd7499c
Mustafa <mustafa_9611@hotmail.com>
2025-11-17 18:04:51 +0100

    feat(deps): add @xyflow/react for flow diagram support

--- src/components/entity-components.tsx
+++ src/components/entity-components.tsx
@@ -55,7 +55,7 @@ type EntityContainerProps = {
 
 export const EntityContainer = ({ header, search, pagination, children }: EntityContainerProps) => {
     return (
-        <div className="p-4 md:px-10 md:py-6 h-full">
+        <div className="p-4 md:px-10 md:py-6 h-full w-full">
             <div className="mx-auto max-w-screen-xl w-full flex flex-col gap-y-8 h-full">
                 {header}
                 <div className="flex flex-col gap-y-4 h-full">
